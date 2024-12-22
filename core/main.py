678c0455de0a57d01dd8e0f710a5a22055b75431
Dev Keshwani <dev.keshwani345@gmail.com>
2024-12-22 18:59:13 +0530

    feat: longest streak

--- core/main.py
+++ core/main.py
@@ -8,11 +8,52 @@ def clean_data(path):
     data.drop(cols_to_drop, axis=1, inplace=True)
     return data
 
+def longestStreak(data):
+    longestStreak = 0
+    streak = 0
+    
+    data['timestamp'] = pd.to_datetime(data['timestamp'], format='ISO8601', errors='coerce')
+    data['date'] = pd.to_datetime(data['timestamp']).dt.date
+
+    data = data.dropna(subset=['date'])
+    unique_dates = sorted(data['date'].unique())
+    
+    for i in range(1, len(unique_dates)):
+        if (unique_dates[i] - unique_dates[i - 1]).days == 1:
+            if streak == 0:
+                temp_start_date = unique_dates[i - 1]
+            streak += 1
+        else:
+            if streak > longestStreak:
+                longestStreak = streak
+                start_date = temp_start_date
+                end_date = unique_dates[i - 1]
+            streak = 0
+            temp_start_date = None
+    
+    if streak > longestStreak:
+        longestStreak = streak
+        start_date = temp_start_date
+        end_date = unique_dates[-1]
+    
+    print(f"Longest Streak: {longestStreak}")
+    print(f"Start Date: {start_date}")
+    print(f"End Date: {end_date}")
+
+
+def monthWiseActivity(data):
+    return None
+def developerActivity(data):
+    return None
+
 def main():
     # data=clean_data("data.csv")
     # data.to_csv("clean_data.csv",index=False)
     data=pd.read_csv("clean_data.csv")
-    print(data.iloc[0])
+    longestStreak(data)
+    monthWiseActivity(data)
+    developerActivity(data)
+
 
 if __name__ == "__main__":  
     main()
